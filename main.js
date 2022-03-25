3e3f5721e9c44a2e04b230de8bb58563e8fd4d25
Jayson Hwang <75832544+jhwang0801@users.noreply.github.com>
2022-03-25 14:25:50 +0900

    Final Version
    add Go-top button

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const header = document.querySelector(".header.container");
+const goTopBtn = document.querySelector(".gotop_btn i");
 
 document.addEventListener("scroll", ()=>{
     var scroll_position = window.scrollY;
     if (scroll_position > 250) {
         header.style.backgroundColor = "#547B97";
+        goTopBtn.style.display = "flex";
     } else {
         header.style.backgroundColor = "transparent";
+        goTopBtn.style.display = "none";
     }
+})
+
+goTopBtn.addEventListener("click", function(){
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
 })
 
 const toogleBtn = document.querySelector(".nav_hamburger");
